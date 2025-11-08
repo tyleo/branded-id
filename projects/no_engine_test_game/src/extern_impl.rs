e53c12990827da858651076dcb39f6d574b6cc1b
tyleo <git@tyleo.com>
2025-11-08 11:35:15 -0500

    no_engine: add some logging to the test game

--- projects/no_engine_test_game/src/extern_impl.rs
+++ projects/no_engine_test_game/src/extern_impl.rs
@@ -6,5 +6,7 @@ use wasm_bindgen::prelude::*;
 #[cfg_attr(not(feature = "wasm"), no_mangle)]
 #[cfg_attr(feature = "wasm", wasm_bindgen)]
 pub extern "C" fn run_with_extern() {
+    println!("`run_with_extern`: called");
     run::<MutWindowCtxPtr>();
+    println!("`run_with_extern`: completed");
 }
--- projects/no_engine_test_game/src/lib.rs
+++ projects/no_engine_test_game/src/lib.rs
@@ -4,12 +4,17 @@ use no_engine::*;
 pub mod extern_impl;
 
 fn run<Ctx: WindowSysCtx>() {
+    println!("`run`: called");
+    println!("`run`: creating ctx");
     let mut ctx = Ctx::new();
 
+    println!("`run`: creating window_sys");
     let mut window_sys = WindowSys::new();
 
+    println!("`run`: retaining window");
     let window_id = window_sys.retain(&mut ctx, 10, 1);
 
+    println!("`run`: coloring window");
     for x in 1..10 {
         let color = if (x % 2) == 0 {
             ColorU8 {
@@ -29,7 +34,9 @@ fn run<Ctx: WindowSysCtx>() {
         unsafe { window_sys.set_pixel_color(&mut ctx, window_id, x, 0, color) }
     }
 
+    println!("`run`: releasing window");
     unsafe {
         window_sys.release(&mut ctx, window_id);
     }
+    println!("`run`: completed");
 }
