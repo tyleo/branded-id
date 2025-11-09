6a8ad3b7db5a642b3f4da202631c22ba7491c320
tyleo <git@tyleo.com>
2025-11-08 20:33:49 -0500

    Remove unnecessary prints

--- projects/no_engine_test_game/src/extern_impl.rs
+++ projects/no_engine_test_game/src/extern_impl.rs
@@ -2,7 +2,5 @@ use {crate::run, no_engine_impl_extern::MutWindowCtxPtr};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn run_with_extern() {
-    println!("`run_with_extern`: called");
     run::<MutWindowCtxPtr>();
-    println!("`run_with_extern`: completed");
 }
